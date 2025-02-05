6439efa76224d8f00414d785a9c4e1c7a0c755df
Marwan Abdalla <marwan3m2000@gmail.com>
2025-02-05 23:46:22 +0100

    added landiong-page component

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -13,7 +13,7 @@ export default function DashboardPage() {
         </div>
         <div className="flex items-center gap-4">
           <h2 className="text-xl">{user?.displayName}</h2>
-          <button onClick={logoutUser} className="btn btn-neutral shadow-none">
+          <button onClick={logoutUser} className="btn btn-outline rounded-lg">
             Sign Out
           </button>
         </div>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -39,7 +39,49 @@ export default function LandingPage() {
       </main>
 
       <div className="flex w-full flex-col items-center justify-center gap-4 lg:flex-row lg:items-end">
-        <div className="bg-neutral h-80 w-54 rounded-3xl"></div>
+        <div className="bg-neutral flex h-80 w-54 flex-col rounded-3xl p-4 text-xs text-neutral-400">
+          <span className="text-xs">Select slot</span>
+          <span className="text-xl text-neutral-50">January 25, 2022</span>
+          <div className="my-4 flex w-full justify-center gap-2 rounded-2xl bg-neutral-900 text-xs">
+            <div className="flex flex-col items-center rounded-xl px-4 py-1">
+              <span>Mon</span>
+              <span className="text-xl text-neutral-100">24</span>
+            </div>
+            <div className="flex flex-col items-center rounded-xl bg-neutral-700 px-4 py-1">
+              <span>Tue</span>
+              <span className="text-xl text-neutral-50">25</span>
+            </div>
+            <div className="flex flex-col items-center rounded-xl px-4 py-1">
+              <span>Wed</span>
+              <span className="text-xl text-neutral-100">26</span>
+            </div>
+          </div>
+          <div className="mb-3 flex w-full items-center gap-2">
+            <span>10:00</span>
+            <div className="flex flex-1 items-center gap-2 rounded-xl bg-teal-100 p-2 text-neutral-800">
+              <input
+                type="checkbox"
+                defaultChecked
+                className="checkbox h-5 w-5"
+              />
+              <span>10:00 - 11:00</span>
+            </div>
+          </div>
+          <div className="mb-3 flex w-full items-center gap-2">
+            <span>11:00</span>
+            <div className="flex flex-1 items-center gap-2 rounded-xl bg-teal-100 p-2 text-neutral-800">
+              <input type="checkbox" className="checkbox h-5 w-5" />
+              <span>11:00 - 12:00</span>
+            </div>
+          </div>
+          <div className="flex w-full items-center gap-2">
+            <span>12:00</span>
+            <div className="flex flex-1 items-center gap-2 rounded-xl bg-teal-100 p-2 text-neutral-800">
+              <input type="checkbox" className="checkbox h-5 w-5" />
+              <span>12:00 - 13:00</span>
+            </div>
+          </div>
+        </div>
         <div
           className="h-52 w-54 rounded-3xl bg-cover bg-center"
           style={{
@@ -48,9 +90,12 @@ export default function LandingPage() {
         ></div>
         <div className="flex h-80 w-54 flex-col items-center justify-between rounded-3xl">
           <div className="bg-base-300 flex w-40 flex-col items-center justify-center gap-1 rounded-xl p-2">
-            <button className="btn btn-neutral w-34 rounded-lg shadow-none">
-              Try it now
-            </button>
+            <Link to="/register">
+              <button className="btn btn-neutral w-34 rounded-lg shadow-none">
+                Try it now
+              </button>
+            </Link>
+
             <span className="text-center text-sm">free for personal use</span>
           </div>
           <div className="flex w-54 justify-center rounded-3xl bg-teal-100 px-4 py-8">
