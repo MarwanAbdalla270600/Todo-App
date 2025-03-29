0229be0b5c6530f144ada908cb555dccca873224
Marwan Abdalla <marwan3m2000@gmail.com>
2025-03-29 05:45:24 +0100

    made course detail page responsive

--- src/pages/CourseDetailsPage.tsx
+++ src/pages/CourseDetailsPage.tsx
@@ -21,29 +21,27 @@ export default function CourseDetailsPage() {
   if (!course) return <div>Loading course...</div>;
 
   return (
-    <div className="flex flex-col gap-12 px-4 py-12">
-      <div className="mx-auto max-w-5xl">
-        <div className="flex flex-wrap items-center justify-between py-12">
-          <div className="flex h-fit max-w-1/2 flex-col gap-6">
-            <h3 className="text-primary text-xl">
-              A Course You'll Actually Finish
-            </h3>
-            <h1 className="text-5xl font-semibold">{course.title}</h1>
-            <h2 className="text-2xl">{course.description}</h2>
-            <div className="flex gap-4">
-              <button className="btn btn-primary rounded-4xl py-6 text-xl text-black">
-                Enroll Now
-              </button>
-              <button className="btn btn-secondary rounded-4xl py-6 text-xl text-black">
-                Preview Course
-              </button>
-            </div>
-          </div>
-          <div className="max-w-1/2">
-            <CourseDetailsCard course={course}></CourseDetailsCard>
+    <main className="mx-auto max-w-5xl px-4 py-12">
+      <div className="flex flex-col items-center justify-between gap-8 py-12 lg:flex-row">
+        <div className="flex h-fit w-full flex-col items-center gap-6 text-center md:items-start md:text-start lg:max-w-1/2">
+          <h3 className="text-primary text-sm sm:text-xl">
+            A Course You'll Actually Finish
+          </h3>
+          <h1 className="text-4xl font-semibold sm:text-5xl">{course.title}</h1>
+          <h2 className="text-xl md:text-2xl">{course.description}</h2>
+          <div className="flex gap-4">
+            <button className="btn btn-primary rounded-4xl py-6 text-xl text-black">
+              Enroll Now
+            </button>
+            <button className="btn btn-secondary rounded-4xl py-6 text-xl text-black">
+              Preview Course
+            </button>
           </div>
         </div>
+        <div className="w-full lg:w-fit lg:max-w-1/2">
+          <CourseDetailsCard course={course}></CourseDetailsCard>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
